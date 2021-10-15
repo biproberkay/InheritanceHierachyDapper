9d39236b80276bea084b638b2c42076aabd9db85
biproberkay <biproberkay@gmail.com>
2021-10-16 00:55:15 +0300

    there is some issues. i will look later.

---
+++ src/InheritanceHierachyDapper/Contract.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceHierachyDapper
+{
+    public abstract class Contract
+    {
+        public int ContractId { get; set; }
+        public DateTime StartDate { get; set; }
+        public int DurationMonths { get; set; }
+        public decimal Charge { get; set; }
+        public ContractType ContractType { get; set; }
+        public int CustomerId { get; set; }
+    }
+    public class MobileContract : Contract
+    {
+        public MobileContract() => ContractType = ContractType.Mobile;
+        public string MobileNumber { get; set; }
+    }
+    public class TvContract : Contract
+    {
+        public TvContract() => ContractType = ContractType.TV;
+        public TVPackageType TVPackageType { get; set; }
+    }
+    public class BroadBandContract : Contract
+    {
+        public BroadBandContract() => ContractType = ContractType.Broadband;
+        public int DownloadSpeed { get; set; }
+    }
+    public enum TVPackageType
+    {
+        S, M, L, XL
+    }
+    public enum ContractType
+    {
+        Mobile = 1, TV, Broadband
+    }
+}
---
+++ src/InheritanceHierachyDapper/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+
+namespace InheritanceHierachyDapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tvContracts = new List<TvContract>();
+            var mobileContracts = new List<MobileContract>();
+            var broadbandContracts = new List<BroadBandContract>();
+            var sql = @"select * from contracts";
+            using (var connection = new SqliteConnection("Data Source = Projects"))
+            {
+                using (var reader = connection.ExecuteReader(sql))
+                {
+                    var tvContractParser = reader.GetRowParser<TvContract>();
+                    var mobileContractParser = reader.GetRowParser<MobileContract>();
+                    var broadbandContractParser = reader.GetRowParser<BroadBandContract>();
+
+                    while (reader.Read())
+                    {
+                        var discriminator = (ContractType)reader.GetInt32(reader.GetOrdinal(nameof(ContractType)));
+                        switch (discriminator)
+                        {
+                            case ContractType.TV:
+                                tvContracts.Add(tvContractParser(reader));
+                                break;
+                            case ContractType.Broadband:
+                                broadbandContracts.Add(broadbandContractParser(reader));
+                                break;
+                            case ContractType.Mobile:
+                                mobileContracts.Add(mobileContractParser(reader));
+                                break;
+                        }
+                    }
+                }
+
+            }
+            Console.WriteLine("TV Contracts");
+            tvContracts.ForEach(c => Console.WriteLine($"Duration: {c.DurationMonths} months, Package Type: {c.TVPackageType.ToString()}"));
+            Console.WriteLine("Broadband Contracts");
+            broadbandContracts.ForEach(c => Console.WriteLine($"Duration: {c.DurationMonths} months, Cost: {c.Charge}, Download: {c.DownloadSpeed} Mbps"));
+            Console.WriteLine("Mobile Contracts");
+            mobileContracts.ForEach(c => Console.WriteLine($"Duration: {c.DurationMonths} months, Number: {c.MobileNumber}"));
+        }
+    }
+}
